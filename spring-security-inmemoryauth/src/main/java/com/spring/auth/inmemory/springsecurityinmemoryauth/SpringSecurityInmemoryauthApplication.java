c14a1d493ffffe33555c4ed0e70667194dd4bbe0
dasar <sagar9dasari@gmail.com>
2020-03-06 20:56:19 -0600

    Adding Spring security in-memory authentication

---
+++ spring-security-inmemoryauth/src/main/java/com/spring/auth/inmemory/springsecurityinmemoryauth/SpringSecurityInmemoryauthApplication.java
@@ -0,0 +1,13 @@
+package com.spring.auth.inmemory.springsecurityinmemoryauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInmemoryauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInmemoryauthApplication.class, args);
+	}
+
+}
---
+++ spring-security-inmemoryauth/src/test/java/com/spring/auth/inmemory/springsecurityinmemoryauth/SpringSecurityInmemoryauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.auth.inmemory.springsecurityinmemoryauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInmemoryauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
